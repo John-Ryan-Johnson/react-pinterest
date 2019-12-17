86a75ad09b9a0b3848d59646d77ea9252f875018
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-12-16 18:56:31 -0600

    whatev

--- src/components/SingleBoard/SingleBoard.js
+++ src/components/SingleBoard/SingleBoard.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import boardsData from '../../helpers/data/boardsData';
-import pinsData from '../../helpers/data/pinsData';
-import Pins from '../Pins/Pins';
+import boardsData from "../../helpers/data/boardsData";
+import pinsData from "../../helpers/data/pinsData";
+import Pins from "../Pins/Pins";
 
 class SingleBoard extends React.Component {
   static propTypes = {
@@ -42,16 +42,14 @@ class SingleBoard extends React.Component {
     const { board, pins } = this.state;
     return (
       <div>
-        <button className="btn btn-info" onClick={this.removeSelectedBoardId}>
+        <button className='btn btn-info' onClick={this.removeSelectedBoardId}>
           x Close Board View
         </button>
-        <div className="SingleBoard col-8 offset-2">
+        <div className='SingleBoard col-8 offset-2'>
           <h2>{board.name}</h2>
           <p>{board.description}</p>
-          <div className="d-flex flex-wrap">
-            {pins.map(pin => (
-              <Pins pin={pin} />
-            ))}
+          <div className='d-flex flex-wrap'>
+            {pins.map(pin => <Pins pin={pin} />)}
           </div>
         </div>
       </div>
