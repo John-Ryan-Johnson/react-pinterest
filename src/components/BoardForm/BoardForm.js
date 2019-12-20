657abc4f71cac28c272d9bf75188c1e3353e0812
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-12-20 12:39:52 -0600

    edit works

--- src/components/BoardForm/BoardForm.js
+++ src/components/BoardForm/BoardForm.js
@@ -2,10 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import authData from '../../helpers/data/authData';
+import boardShape from '../../helpers/propz/boardShape';
 
 class BoardForm extends React.Component {
   static propTypes = {
     addBoard: PropTypes.func,
+    boardToEdit: boardShape.boardShape,
+    editMode: PropTypes.bool,
+    updateBoard: PropTypes.func,
   }
 
   state = {
@@ -13,11 +17,24 @@ class BoardForm extends React.Component {
     boardDescription: '',
   }
 
-  saveBoardEvent = (e) => {
-    const { addBoard } = this.props
+  componentDidMount() {
+    const { boardToEdit, editMode } = this.props;
+    if (editMode) {
+      this.setState({ boardName: boardToEdit.name, boardDescription: boardToEdit.description });
+    }
+  }
 
-;    e.preventDefault();
-     const newBoard = {
+  componentDidUpdate(prevProps) {
+    if ((prevProps.boardToEdit.id !== this.props.boardToEdit.id) && this.props.editMode) {
+      this.setState({ boardName: this.props.boardToEdit.name, boardDescription: this.props.boardToEdit.description });
+    }
+  }
+
+  saveBoardEvent = (e) => {
+    const { addBoard } = this.props;
+
+    e.preventDefault();
+    const newBoard = {
       name: this.state.boardName,
       description: this.state.boardDescription,
       uid: authData.getUid(),
@@ -26,9 +43,20 @@ class BoardForm extends React.Component {
     this.setState({ boardName: '', boardDescription: '' });
   }
 
+  updateBoardEvent = (e) => {
+    e.preventDefault();
+    const { updateBoard, boardToEdit } = this.props;
+    const updatedBoard = {
+      name: this.state.boardName,
+      description: this.state.boardDescription,
+      uid: boardToEdit.uid,
+    };
+    updateBoard(boardToEdit.id, updatedBoard);
+  }
+
   nameChange = (e) => {
     e.preventDefault();
-    this.setState({ boardName: e.target.value })
+    this.setState({ boardName: e.target.value });
   }
 
   descriptionChange = (e) => {
@@ -37,31 +65,36 @@ class BoardForm extends React.Component {
   }
 
   render() {
+    const { editMode } = this.props;
+
     return (
       <form className='col-6 offset-3 BoardForm'>
-        <div className='form-group'>
-          <label htmlFor='order-name'>Board Name:</label>
+        <div className="form-group">
+          <label htmlFor="order-name">Board Name:</label>
           <input
-            type='text'
-            className='form-control'
-            id='board-name'
-            placeholder='Enter board name'
+            type="text"
+            className="form-control"
+            id="board-name"
+            placeholder="Enter board name"
             value={this.state.boardName}
             onChange={this.nameChange}
           />
         </div>
-        <div className='form-group'>
-          <label htmlFor='description-name'>Board Description:</label>
+        <div className="form-group">
+          <label htmlFor="description-name">Board Description:</label>
           <input
-            type='text'
-            className='form-control'
-            id='board-description'
-            placeholder='Enter board description'
+            type="text"
+            className="form-control"
+            id="board-description"
+            placeholder="Enter board description"
             value={this.state.boardDescription}
             onChange={this.descriptionChange}
           />
         </div>
-        <button className='btn btn-secondary' onClick={this.saveBoardEvent}> => Save Board</button>
+        {
+          (editMode) ? (<button className="btn btn-warning" onClick={this.updateBoardEvent}>Update Board</button>)
+            : (<button className="btn btn-secondary" onClick={this.saveBoardEvent}>Save Board</button>)
+        }
       </form>
     );
   }
--- src/components/BoardsContainer/BoardsContainer.js
+++ src/components/BoardsContainer/BoardsContainer.js
@@ -2,11 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Board from '../Board/Board';
+import BoardForm from '../BoardForm/BoardForm';
 
 import authData from '../../helpers/data/authData';
-import boardsData from '../../helpers/data/boardData';
 import boardData from '../../helpers/data/boardData';
-import BoardForm from '../BoardForm/BoardForm';
 
 
 class BoardsContainer extends React.Component {
@@ -16,6 +15,9 @@ class BoardsContainer extends React.Component {
 
   state = {
     boards: [],
+    editMode: false,
+    boardToEdit: {},
+    showBoardForm: false,
   }
 
   componentDidMount() {
@@ -35,8 +37,30 @@ class BoardsContainer extends React.Component {
     boardData.saveBoard(newBoard)
     .then(() => {
       this.getBoards();
+      this.setState({ showBoardForm: false });
     })
     .catch((errorFromSaveBoard) => console.error({ errorFromSaveBoard }));
+  }
+
+  updateBoard = (boardId, updatedBoard) => {
+    boardData.updateBoard(boardId, updatedBoard)
+      .then(() => {
+        this.getBoards();
+        this.setState({ editMode: false, showBoardForm: false });
+      })
+      .catch((errorFromUpdateBoard) => console.error({ errorFromUpdateBoard }));
+  }
+
+  setEditMode = (editMode) => {
+    this.setState({ editMode, showBoardForm: true });
+  }
+
+  setBoardToEdit = (board) => {
+    this.setState({ boardToEdit: board });
+  }
+
+  setShowBoardForm = () => {
+    this.setState({ showBoardForm: true });
   }
 
   render() {
@@ -44,8 +68,9 @@ class BoardsContainer extends React.Component {
 
     return (
       <div>
-        <BoardForm addBoard={this.addBoard} />
-        {this.state.boards.map((board) => (<Board key={board.id} board={board} setSingleBoard={setSingleBoard} />))}
+         <button onClick={this.setShowBoardForm}>Add a new board</button>
+        { this.state.showBoardForm && <BoardForm addBoard={this.addBoard} editMode={this.state.editMode} boardToEdit={this.state.boardToEdit} updateBoard={this.updateBoard} /> }
+        {this.state.boards.map((board) => (<Board key={board.id} board={board} setSingleBoard={setSingleBoard} setEditMode={this.setEditMode} setBoardToEdit={this.setBoardToEdit} />))}
       </div>);
   }
 }
